d772a0e231a39975012746eed72c5281eae09144
suphattra2542 <87429331+suphattra2542@users.noreply.github.com>
2021-09-19 14:13:17 +0700

    wed

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+        
+        <div class="d-flex" id="wrapper">
+            <!-- Sidebar-->
+           
+            <div class="border-end bg-white" id="sidebar-wrapper">
+<body>
+                    <div class="display-3 text-center">หน้าแรก</div> 
+                         <div class="list-group list-group-flush">
+                             <a href="index1.php" class="btn btn-success mb-3">ข้อมูลผู้ใช้</a>
+                             <a href="index2.php" class="btn btn-success mb-3">ข้อมูลสัตว์</a>
+                             <a href="index3.php" class="btn btn-success mb-3">การจองคิว</a>
+</body>
+                
+    
+                
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width-, initial-scale=1.0">
+  
+
+    <link rel="stylesheet" href="bootstrap/bootstrap.css">
+</head>
+<body>
+
+           
+
+       
+    
+
+    <script src="js/slim.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
